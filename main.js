fcb29f1978a441cb6743137dc2ffdc138775be98
88Thomas <73180506+88Thomas@users.noreply.github.com>
2020-10-20 16:35:21 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-let myImage = document.querySelector('img');
-myImage.addEventListener('click', function() {
-    let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/franco.jpg') {
-      myImage.setAttribute('src', 'images/jose-sanjurgo.jpg');
-    } else {
-      myImage.setAttribute('src', 'images/franco.jpg');
-    }
-});
