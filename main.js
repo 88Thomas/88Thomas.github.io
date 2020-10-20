dd55206f1f841511dcf0162acab38753b192cdad
88Thomas <73180506+88Thomas@users.noreply.github.com>
2020-10-20 16:42:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let myImage = document.querySelector('img');
 myImage.addEventListener('click', function() {
     let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/franco.jpg') {
-      myImage.setAttribute('src', 'images/jose-sanjurgo.jpg');
+    if (mySrc === 'franco.jpg') {
+      myImage.setAttribute('src', 'jose-sanjurgo.jpg');
     } else {
-      myImage.setAttribute('src', 'images/franco.jpg');
+      myImage.setAttribute('src', 'franco.jpg');
     }
 });
