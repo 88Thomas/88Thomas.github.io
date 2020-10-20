cf08e385d76bc04a66e7dfd84b9482be261297de
88Thomas <thomas.fr824@gmail.com>
2020-10-20 23:39:26 +0200

    initial commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let myImage = document.querySelector('img');
 myImage.addEventListener('click', function() {
     let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'BrocanteLuzy.github.io/franco.jpg') {
-      myImage.setAttribute('src', 'BrocanteLuzy.github.io/jose-sanjurgo.jpg');
+    if (mySrc === 'franco.jpg') {
+      myImage.setAttribute('src', 'jose-sanjurgo.jpg');
     } else {
-      myImage.setAttribute('src', 'BrocanteLuzy.github.io/franco.jpg');
+      myImage.setAttribute('src', 'franco.jpg');
     }
 });
